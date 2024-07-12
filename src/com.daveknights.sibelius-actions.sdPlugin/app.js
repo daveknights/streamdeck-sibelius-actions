681cff1835e1a5fcfcb6666c33abdb21bcc9ed24
David Knights <dave.knights@virginmedia.com>
2024-07-12 22:30:08 +0100

    Create function to connect & run either plugin or command

--- src/com.daveknights.sibelius-actions.sdPlugin/app.js
+++ src/com.daveknights.sibelius-actions.sdPlugin/app.js
@@ -6,11 +6,9 @@ const cmdRunner = new Action('com.daveknights.cmd-runner.action');
 const globalContext = 'comSibeliusActionsContext';
 const connectMsg = {
     "message": "connect",
-    "clientName": "StreamDeckPlgRunner",
+    "clientName": "StreamDeckActionsRunner",
     "handshakeVersion": "1.0",
-    "plugins": [],
 };
-let globalSettings = {};
 let sibsocket = '';
 let sibeliusToken = '';
 let allPluginNames = [];
@@ -23,95 +21,61 @@ $SD.onConnected(({ actionInfo, appInfo, connection, messageType, port, uuid }) =
 });
 
 $SD.onDidReceiveGlobalSettings(({payload}) => {
-    globalSettings = {...payload.settings.payload};
-
     if (payload.settings?.payload?.plugins) {
         connectMsg.plugins = payload.settings.payload.pluginList;
-    }
 
-    if (payload.settings?.payload?.token) {
-        sibeliusToken = payload.settings.payload.token;
-    } else {
         sibsocket.readyState && sibsocket.close();
         sibeliusToken = '';
     }
 });
 
-plgRunner.onKeyUp(({ action, context, device, event, payload }) => {
+const connectAndRun = (type, payload) => {
+    let name = '';
+
+    if (type === 'plugin') {
+        name = payload.name;
+    } else {
+        name = payload.commands[0];
+    }
+
     if (!sibsocket || sibsocket.readyState === WebSocket.CLOSED) {
         sibsocket = new WebSocket('ws://127.0.0.1:1898');
     }
 
-    if (payload.settings && payload.settings.payload.pluginName) {
+    if (name) {
         if (sibeliusToken) {
-            sibsocket.send(JSON.stringify({ "message" : "invokePlugin", "name": payload.settings.payload.pluginName }));
+            sibsocket.send(JSON.stringify(payload));
         } else {
-            sibsocket.onopen = () => sibsocket.send(JSON.stringify(connectMsg));
+            sibsocket.onopen = () => {
+                sibsocket.send(JSON.stringify(connectMsg));
+
+                sibsocket.onclose = () => {
+                    sibsocket = '';
+                    sibeliusToken = '';
+                }
+            }
         }
 
         sibsocket.onmessage = event => {
             const jsonObj = JSON.parse(event.data);
 
-            if (jsonObj.message === 'connectResponse') {
-                sibsocket.send(JSON.stringify({ "message" : "invokePlugin", "name": payload.settings.payload.pluginName }));
-            }
+            if (jsonObj.message === 'connectResponse' && jsonObj.result === true) {
+                sibeliusToken = jsonObj.sessionToken;
 
-            if(jsonObj.sessionToken) {
-                globalSettings.token = jsonObj.sessionToken;
-                $SD.setGlobalSettings({context: globalContext, payload: globalSettings});
-
-                $SD.getGlobalSettings(globalContext);
+                sibsocket.send(JSON.stringify(payload));
             }
         }
     } else {
         $SD.showAlert(context);
     }
-});
+};
 
-plgRunner.onSendToPlugin(({context, payload}) => {
-    if (payload) {
-        $SD.setSettings(context, payload);
-    }
-});
+plgRunner.onKeyUp(({ action, context, device, event, payload }) => payload.settings &&
+    connectAndRun('plugin', { "message" : "invokePlugin", "name": payload.settings.payload.pluginName }));
 
-cmdRunner.onKeyUp(({ action, context, device, event, payload }) => {
-    if (!sibsocket || sibsocket.readyState === WebSocket.CLOSED) {
-        sibsocket = new WebSocket('ws://127.0.0.1:1898');
-        sibeliusToken = '';
-    }
+plgRunner.onSendToPlugin(({context, payload}) => payload && $SD.setSettings(context, payload));
 
-    if (payload.settings && payload.settings.payload.commandName) {
-        if (sibeliusToken) {
-            sibsocket.send(JSON.stringify({ "message" : "invokeCommands", "commands": [payload.settings.payload.commandName] }));
-        } else {
-            sibsocket.onopen = () => sibsocket.send(JSON.stringify(connectMsg));
-        }
+cmdRunner.onKeyUp(({ action, context, device, event, payload }) => payload.settings &&
+    connectAndRun('command', { "message" : "invokeCommands", "commands": [payload.settings.payload.commandName] }));
 
-        sibsocket.onmessage = event => {
-            const jsonObj = JSON.parse(event.data);
-
-            if (jsonObj.message === 'connectResponse') {
-                sibsocket.send(JSON.stringify({ "message" : "invokeCommands", "commands": [payload.settings.payload.commandName] }));
-            }
-
-            if(jsonObj.sessionToken) {
-                globalSettings.token = jsonObj.sessionToken;
-                $SD.setGlobalSettings({context: globalContext, payload: globalSettings});
-
-                $SD.getGlobalSettings(globalContext);
-            }
-        }
-    } else {
-        $SD.showAlert(context);
-    }
-});
-
-cmdRunner.onSendToPlugin(({context, payload}) => {
-    if (payload) {
-        $SD.setSettings(context, payload);
-
-        sibsocket && sibsocket.close();
-        sibsocket = '';
-        sibeliusToken = '';
-    }
-});
+cmdRunner.onSendToPlugin(({context, payload}) => payload && $SD.setSettings(context, payload));
